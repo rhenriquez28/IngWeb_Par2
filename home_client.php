d7543aed2931f0698d2d95f99d2113a2b47c629e
Roy Henriquez <roy.henriquez@gmail.com>
2017-10-31 07:53:58 -0500

    Agregar home_client.php

---
+++ home_client.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Cliente: Root</p>
+    <p>Numero de Cuenta: 01-345-2124-2233</p>
+    <br>
+    <table>
+      <tr>
+        <th>Nº</th>
+        <th>Tipo de Transaccion</th>
+        <th>Monto</th>
+        <th>Saldo Capital</th>
+      </tr>
+      <?php if () {
+        # code...
+      } ?>
+    </table>
+  </body>
+</html>
