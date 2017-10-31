63e8d6267f304ee46af62270bfbfa3dac5db1115
Roy Henriquez <roy.henriquez@gmail.com>
2017-10-31 09:00:47 -0500

    Cambio nombre

---
+++ report.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Cliente: Root</p>
+    <p>Numero de Cuenta: 01-345-2124-2233</p>
+    <br>
+    <table>
+      <thead>
+        <th>Nº</th>
+        <th>Tipo de Transaccion</th>
+        <th>Monto</th>
+        <th>Saldo Capital</th>
+      </thead>
+      <?php if ($new_tran==true ) {
+        # code...
+      } ?>
+    </table>
+    <br>
+    <br>
+    <table>
+      <thead>
+        <th>Total de Credito (Depósitos)</th>
+        <th>Total de Debito (Retiros)</th>
+        <th>Total de Transferencias</th>
+        <th>Total de Intereses ganados</th>
+        <th>Saldo Capital</th>
+      </thead>
+      <tr>
+        <td></td>
+      </tr>
+    </table>
+  </body>
+</html>
